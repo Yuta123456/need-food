f1d6bf7aa943c9da85105e393fa83810d1c4a531
Yuta123456 <yuuta09090530@icloud.com>
2022-09-18 23:39:24 +0900

    data

---
+++ data/dummyData.ts
@@ -0,0 +1,50 @@
+import { MealSchedule } from "../pages";
+
+const dummyData: MealSchedule = {
+    "09/17": {
+      breakFast: true,
+      lunch: true,
+      dinner: true,
+    },
+    "09/18": {
+      breakFast: true,
+      lunch: true,
+      dinner: true,
+  },
+  "09/19": {
+    breakFast: true,
+    lunch: true,
+    dinner: true,
+  },
+  "09/20": {
+    breakFast: true,
+    lunch: true,
+    dinner: true,
+  },
+  "09/21": {
+    breakFast: true,
+    lunch: true,
+    dinner: true,
+  },
+  "09/22": {
+    breakFast: true,
+    lunch: true,
+    dinner: true,
+  },
+  "09/23": {
+    breakFast: true,
+    lunch: true,
+    dinner: true,
+  },
+  "09/24": {
+    breakFast: true,
+    lunch: false,
+    dinner: true,
+  },
+  "09/25": {
+    breakFast: true,
+    lunch: false,
+    dinner: true,
+  },
+};
+export default dummyData;
