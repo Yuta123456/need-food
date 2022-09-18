fa7f4bc67701328f8d90cc4e5ad36783d7b20c29
Yuta123456 <yuuta09090530@icloud.com>
2022-09-18 15:48:17 +0900

    設定ファイル

---
+++ .prettier.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
