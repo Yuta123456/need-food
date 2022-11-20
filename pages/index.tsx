3f8bad0b033fab0588e0c82675bf7b875019e83e
Yuta123456 <yuuta09090530@icloud.com>
2022-11-20 14:17:13 +0900

    loading実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,8 @@ import { useRouter } from "next/router";
 import { auth as adminAuth } from "../firebaseAdmin";
 import nookies from "nookies";
 import { GetServerSideProps, NextPage } from "next";
-
+import { Center, Spinner } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 const fetcher = (url: string) =>
   fetch(url).then((res) => {
     return res.json();
@@ -33,7 +34,7 @@ const Home: NextPage<{ uid: string }> = ({ uid }) => {
     })
       .then(async (res) => {
         const mealJson = await res.json();
-        setMealSchedule(mealJson.mealSchedule);
+        // setMealSchedule(mealJson.mealSchedule);
       })
       .catch((err) => {
         console.log(err);
@@ -63,7 +64,18 @@ const Home: NextPage<{ uid: string }> = ({ uid }) => {
           />
         </>
       ) : (
-        <p>loading...</p>
+        <Center h="100vh" color="white" sx={{ "flex-flow": "column" }}>
+          <Text color="#4FD1C5" fontSize="3xl">
+            loading...
+          </Text>
+          <Spinner
+            size="xl"
+            speed="1.0s"
+            color="#4FD1C5"
+            emptyColor="gray.200"
+            thickness="4px"
+          />
+        </Center>
       )}
     </>
   );
